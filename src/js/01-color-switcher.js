73153c75528d2655df6e89a75e0ce338c1c0831d
Denys Strashniuk <denysstrashniuk@MacBook-Pro-Denys-Strashniuk.local>
2023-05-10 22:32:21 +0200

    add 01-color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const refs = {
+    startBtnEl: document.querySelector("button[data-start]"),
+    stopBtnEl: document.querySelector("button[data-stop]"),
+}
 
+let intervalId = null;
+
+refs.stopBtnEl.setAttribute("disabled", "");
+
+refs.startBtnEl.addEventListener("click", () => {
+    getAttrDisabled(refs.startBtnEl, refs.stopBtnEl);
+    intervalId = setInterval(renderBodyColor, 1000)
+})
+
+refs.stopBtnEl.addEventListener("click", () =>{
+    getAttrDisabled(refs.stopBtnEl, refs.startBtnEl);
+    clearInterval(intervalId);
+})
+
+function getAttrDisabled(setDisabled, removeDisabled){
+    setDisabled.setAttribute("disabled", "");
+    removeDisabled.removeAttribute("disabled");
+}
+function renderBodyColor(){
+    document.body.style.backgroundColor = getRandomHexColor();
+}
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
