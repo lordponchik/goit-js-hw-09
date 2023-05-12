dc4699722a7eaab1a1b560153d4101a4a306a121
Denys Strashniuk <denysstrashniuk@MacBook-Pro-Denys-Strashniuk.local>
2023-05-12 18:02:03 +0200

    add 02-timer v1.0

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,74 +1,107 @@
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-
-const refs= {
-    timePickerEl: document.querySelector("input#datetime-picker"),
-    startBtnEl: document.querySelector("button[data-start]"),
-    daysEl: document.querySelector("span[data-days]"),
-    hoursEl: document.querySelector("span[data-hours]"),
-    minutesEl: document.querySelector("span[data-minutes]"),
-    secondsEl: document.querySelector("span[data-seconds]"),
-}
+const refs = {
+  timePickerEl: document.querySelector('input#datetime-picker'),
+  startBtnEl: document.querySelector('button[data-start]'),
+  daysEl: document.querySelector('span[data-days]'),
+  hoursEl: document.querySelector('span[data-hours]'),
+  minutesEl: document.querySelector('span[data-minutes]'),
+  secondsEl: document.querySelector('span[data-seconds]'),
+};
 
 const optionsTimePicker = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
-        if(selectedDates[0]<=Date.now()){
-         refs.startBtnEl.setAttribute("disabled", "");
-         alert("Please choose a date in the future")
-         return;
-        }
-        refs.startBtnEl.removeAttribute("disabled");
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
 
-      console.log(selectedDates[0]);
-    },
-  };
+  onClose(selectedDates) {
+    if (selectedDates[0] < Date.now()) {
+      accessBtnStart(false);
+      Notify.warning('Please choose a date in the future');
+      return;
+    }
+    accessBtnStart(true);
+  },
+};
 
 const calendar = flatpickr(refs.timePickerEl, optionsTimePicker);
 
-refs.startBtnEl.setAttribute("disabled", "");
+accessBtnStart(false);
+let intervalId = null;
 
-refs.startBtnEl.addEventListener("click", ()=>{
-    refs.startBtnEl.setAttribute("disabled", "");
-    renderTimer();
-    const intervalId = setInterval(() => {
-        renderTimer();
+refs.startBtnEl.addEventListener('click', startInterval);
 
-    }, 1000);
+function startInterval() {
+  if (calendar.selectedDates[0] < Date.now()) {
+    accessBtnStart(false);
+    Notify.warning('Please choose a date in the future');
+    return;
+  }
+  accessBtnStart(false);
 
-    
-})
+  intervalId = setInterval(() => {
+    renderTimerElement();
+  }, 1000);
+}
+function accessBtnStart(value) {
+  if (!value) {
+    refs.startBtnEl.setAttribute('disabled', '');
+    return;
+  }
+  refs.startBtnEl.removeAttribute('disabled');
+}
 
-function renderTimer(){
-    const {days, hours, minutes, seconds} = convertMs(calendar.selectedDates[0]-Date.now());
+function renderTimerElement() {
+  const { days, hours, minutes, seconds } = convertMs(
+    calendar.selectedDates[0] - Date.now()
+  );
+  const checkTimeUpToMinutes = days === 0 && hours === 0 && minutes === 0;
 
-    refs.daysEl.textContent = days;
-    refs.hoursEl.textContent = hours;
-    refs.minutesEl.textContent = minutes;
-    refs.secondsEl.textContent = seconds;
+  refs.daysEl.textContent = addLeadingZero(days);
+  refs.hoursEl.textContent = addLeadingZero(hours);
+  refs.minutesEl.textContent = addLeadingZero(minutes);
+  refs.secondsEl.textContent = addLeadingZero(seconds);
+
+  if (checkTimeUpToMinutes && seconds <= 10) {
+    animLastSeconds();
+  }
+
+  if (checkTimeUpToMinutes && seconds === 0) {
+    clearInterval(intervalId);
+  }
 }
 
 function convertMs(ms) {
-    // Number of milliseconds per unit of time
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-  
-    // Remaining days
-    const days = Math.floor(ms / day);
-    // Remaining hours
-    const hours = Math.floor((ms % day) / hour);
-    // Remaining minutes
-    const minutes = Math.floor(((ms % day) % hour) / minute);
-    // Remaining seconds
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-  
-    return { days, hours, minutes, seconds };
-  }
-  
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
 
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function animLastSeconds() {
+  const anim = [{ color: 'inherit' }, { color: 'red' }];
+  const timing = {
+    duration: 250,
+    iterations: 1,
+  };
+  refs.secondsEl.animate(anim, timing);
+}
